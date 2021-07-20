072efe9319ac7efa0711c6cbd2d6c3519774e842
Abhibha Gupta <abhibha1807@gmail.com>
2021-07-20 16:33:18 +0530

    added job and train.sh files

---
+++ cnn/train.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#/abhibha-volume/rbt/bin python 
+#
+nvidia-smi
+pwd
+source activate /abhibha-volume/darts
+conda info
+which python
+python /abhibha-volume/darts-xray/cnn/train.py
